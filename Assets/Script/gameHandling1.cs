77fc316f06b9bc101766cd53c315488fe97bd34d
josherz94 <josherz94@gmail.com>
2020-05-12 16:31:50 -0700

    Alpha 4 - including gameHandler

---
+++ Assets/Script/gameHandling1.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Playniax.Ignition.Framework;
+using Playniax.Ignition.SpriteSystem;
+
+
+public class gameHandling : MonoBehaviour
+{
+    public int score_next_scene;
+    public bool boss_scene;
+    // Start is called before the first frame update
+    void Update()
+    {
+        gameOver();
+        nextLevel();    
+    }
+    public void gameOver()
+    {
+        
+        if (GameObject.Find("Player") == null || GameObject.Find("Player").GetComponent<CollisionData>().structuralIntegrity <= 0)
+        {
+            int level = SceneManager.GetSceneByPath("Assets/Scenes/GameOver.unity").buildIndex;
+            SceneManager.LoadScene(3);
+        }
+    }
+    public void nextLevel()
+    {
+        if (PlayerData.Get(0).scoreboard >= score_next_scene && !boss_scene)
+        {
+            int level =  SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(level + 1);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    
+}
